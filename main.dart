2a8a7d3a51a483f7095075aac670dffa76eab1d5
ibnuazman <ibnuazman.11@gmail.com>
2021-11-20 01:04:24 +0800

    I change from using 2 dart file to only 1 dart file which is main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutterlist/textinput.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,14 +10,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late int valueInt;
+
+  late int _fruitsIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    var fruits = ['No fruit', 'apple', 'orange', 'rambutan'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Fruit List'),
+          title: const Text('Fruit List'),
         ),
-        body: TextBar(),
+        body: Column(children: [
+          const Padding(padding: EdgeInsets.all(20)),
+          TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a number to display the list item'),
+              onChanged: (value) {
+                valueInt = int.parse(value);
+                print("The value entered is : $valueInt");
+              }),
+          RaisedButton(
+              child: Text('Submit'),
+              elevation: 8,
+              onPressed: () => {
+                    setState(() {
+                      _fruitsIndex = valueInt;
+                    })
+                  }),
+          Text(fruits.elementAt(_fruitsIndex))
+        ]),
       ),
     );
   }
